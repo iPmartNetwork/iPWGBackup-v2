0712a896ded75fa7199cb97d651b28a9c2dece02
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2025-12-24 00:49:57 +0330

    Create checksum.py

---
+++ security/checksum.py
@@ -0,0 +1,8 @@
+mport hashlib
+
+def sha256sum(file_path: str) -> str:
+    sha = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for chunk in iter(lambda: f.read(8192), b""):
+            sha.update(chunk)
+    return sha.hexdigest()
